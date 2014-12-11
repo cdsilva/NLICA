8e6dea17d41c45c3399deecc8d0e44c79148d687
cdsilva <carmeline.dsilva@gmail.com>
2014-12-11 16:42:49 -0500

    updated swiss rolls example

--- chemotaxis/swissroll.m
+++ chemotaxis/swissroll.m
@@ -50,53 +50,82 @@ close all
 
 load swissroll1.mat
 
-return
-
-figure;
+make_fig(3,3);
 plot3(x,y,z,'.')
+xlabel('x')
+ylabel('y')
+zlabel('z')
 axis equal
 print('swissroll1.eps','-depsc')
+% 
+% figure;
+% bar(res(2:end))
+% ylabel('cross-validation error')
+% print('swissroll1_cv.eps','-depsc')
+% 
+% figure;
+% scatter3(x,y,z,50,V(:,2),'.')
+% axis equal
+% print('swissroll1_color1.eps','-depsc')
+% 
+% figure;
+% scatter3(x,y,z,50,V(:,3),'.')
+% axis equal
+% print('swissroll1_color2.eps','-depsc')
 
-figure;
-bar(res(2:end))
-ylabel('cross-validation error')
-print('swissroll1_cv.eps','-depsc')
-
-figure;
-scatter3(x,y,z,50,V(:,2),'.')
-axis equal
-print('swissroll1_color1.eps','-depsc')
-
-figure;
-scatter3(x,y,z,50,V(:,3),'.')
-axis equal
-print('swissroll1_color2.eps','-depsc')
+make_fig(3,3);
+for i=2:size(D,1)
+    bar(i-1, D(i,i), 'facecolor', (1-min(res(i),1))*[1 1 0]+[0 0 1])
+    hold on
+end
+set(gca, 'ylim', [0.9 1])
+colormap([linspace(1, 0, 64)' linspace(1, 0, 64)' ones(64, 1)])
+colorbar
+caxis([0 1])
+set(gca, 'xtick', (2:size(D,1))-1)
+xlabel('k')
+ylabel('\lambda_k')
+axis square
+print('swissroll1_evals.eps','-depsc')
 
 %%
 
 load swissroll2.mat
+% 
+% figure;
+% plot3(x,y,z,'.')
+% axis equal
+% print('swissroll2.eps','-depsc')
+% 
+% figure;
+% bar(res(2:end))
+% ylabel('cross-validation error')
+% print('swissroll2_cv.eps','-depsc')
+% 
+% figure;
+% scatter3(x,y,z,50,V(:,2),'.')
+% axis equal
+% print('swissroll2_color1.eps','-depsc')
+% 
+% figure;
+% scatter3(x,y,z,50,V(:,6),'.')
+% axis equal
+% print('swissroll2_color2.eps','-depsc')
 
-figure;
-plot3(x,y,z,'.')
-axis equal
-print('swissroll2.eps','-depsc')
 
-figure;
-bar(res(2:end))
-ylabel('cross-validation error')
-print('swissroll2_cv.eps','-depsc')
-
-figure;
-scatter3(x,y,z,50,V(:,2),'.')
-axis equal
-print('swissroll2_color1.eps','-depsc')
-
-figure;
-scatter3(x,y,z,50,V(:,6),'.')
-axis equal
-print('swissroll2_color2.eps','-depsc')
-
-
-
+make_fig(3,3);
+for i=2:size(D,1)
+    bar(i-1, D(i,i), 'facecolor', (1-min(res(i),1))*[1 1 0]+[0 0 1])
+    hold on
+end
+set(gca, 'ylim', [0.9 1])
+colormap([linspace(1, 0, 64)' linspace(1, 0, 64)' ones(64, 1)])
+colorbar
+caxis([0 1])
+set(gca, 'xtick', (2:size(D,1))-1)
+xlabel('k')
+ylabel('\lambda_k')
+axis square
+print('swissroll2_evals.eps','-depsc')
 
 
