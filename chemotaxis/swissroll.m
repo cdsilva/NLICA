970752ae295930cf4bbc28114d8eacd9fd6d1758
cdsilva <carmeline.dsilva@gmail.com>
2014-12-11 16:41:37 -0500

    moved old + new text to chemotaxis/detecting harmonics draft

--- chemotaxis/swissroll.m
+++ chemotaxis/swissroll.m
@@ -2,53 +2,55 @@ clear all
 close all
 
 %%
-
-N = 1500; % number of points considered
-t = rand(1,N);
-t = sort(4*pi*sqrt(t))'; 
-
-h = 15*pi;
-
-z = h*rand(N,1); % random heights
-x = (t+.1).*cos(t);
-y = (t+.1).*sin(t);
-data = [x,y,z]; % data
-
-%%
-figure;
-plot3(x,y,z,'.')
-
-axis equal
-
-
-%%
-
-W = squareform(pdist(data)).^2;
-eps = 5;
-
-neigs = 10;
-
-[V, D] = dmaps(W, eps, neigs);
-
-
-%%
-figure;
-scatter3(x,y,z,50,V(:,2),'.')
-
-figure;
-scatter3(x,y,z,50,V(:,3),'.')
-
-%%
-
-eps_med_scale = 3;
-res = compute_residuals_DMAPS(V, eps_med_scale);
-
-figure;
-plot(res, 'o-')
+% 
+% N = 1500; % number of points considered
+% t = rand(1,N);
+% t = sort(4*pi*sqrt(t))'; 
+% 
+% h = 15*pi;
+% 
+% z = h*rand(N,1); % random heights
+% x = (t+.1).*cos(t);
+% y = (t+.1).*sin(t);
+% data = [x,y,z]; % data
+% 
+% %%
+% figure;
+% plot3(x,y,z,'.')
+% 
+% axis equal
+% 
+% 
+% %%
+% 
+% W = squareform(pdist(data)).^2;
+% eps = 5;
+% 
+% neigs = 10;
+% 
+% [V, D] = dmaps(W, eps, neigs);
+% 
+% 
+% %%
+% figure;
+% scatter3(x,y,z,50,V(:,2),'.')
+% 
+% figure;
+% scatter3(x,y,z,50,V(:,3),'.')
+% 
+% %%
+% 
+% eps_med_scale = 3;
+% res = compute_residuals_DMAPS(V, eps_med_scale);
+% 
+% figure;
+% plot(res, 'o-')
 
 %%
 
 load swissroll1.mat
+
+return
 
 figure;
 plot3(x,y,z,'.')
