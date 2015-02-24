cbdabe50d4a0e7c0d36dfb87ed22ee05b7d11de6
cdsilva <carmeline.dsilva@gmail.com>
2015-02-24 10:26:53 -0500

    update dpaper

--- chemotaxis/local_linear_regression.m
+++ chemotaxis/local_linear_regression.m
@@ -9,9 +9,10 @@ W = exp(-K.^2 / eps^2);
 L = zeros(n);
 for i=1:n
     Xx = [ones(size(X,1),1) X-repmat(X(i,:), n, 1)];
-    Wx = diag(W(i,:));
-%     A = inv(Xx'*Wx*Xx)*Xx'*Wx;
-    A = (Xx'*Wx*Xx)\(Xx'*Wx);
+%     Wx = diag(W(i,:));
+%     A = (Xx'*Wx*Xx)\(Xx'*Wx);
+    Xx2 = Xx'.*repmat(W(i,:), size(Xx, 2), 1);
+    A = (Xx2*Xx)\Xx2;
     L(i,:) = A(1,:);
 end
 
